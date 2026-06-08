a7193f385a92831115830c9e2595592b710e4417
antoinealbrecht <antoinealbrecht21@gmail.com>
2026-06-08 02:06:20 -0400

    Revalidate weight page after saving entry

--- src/server/weight.ts
+++ src/server/weight.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { prisma } from "../lib/prisma";
 
 export async function createWeightEntry(weightLb: number) {
@@ -8,4 +9,6 @@ export async function createWeightEntry(weightLb: number) {
       weightLb,
     },
   });
+
+  revalidatePath("/weight");
 }
